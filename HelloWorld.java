cce74c61861b4597fc04de1792a7c67524b99e10
NANCY-ATIENO <atienonancy235@gmail.com>
2026-02-18 15:56:35 +0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.nancy.practice;
+
+// This is a Marker Interface
+interface MyFirstInterface { }
+
+public class HelloWorld implements MyFirstInterface {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub! I am Nancy Atieno, a CS Student.");
+    }
+}
